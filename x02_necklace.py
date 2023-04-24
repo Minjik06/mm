dd2539d23a52d057c21aadf56d6baf0b2284d379
Minjik06 <minjik525@deltalearns.ca>
2023-04-24 11:20:57 -0700

    21

--- x02_necklace.py
+++ x02_necklace.py
@@ -7,16 +7,28 @@ extra: What is the shortest necklace number sequence that can be made?
 """
 
 def necklace(a,b):
-  """
-  inputs: 
-  a : int value [0..9]
-  b : int value [0..9]
+  a=int(a)
+  b=int(b)
+  c= [a,b]
+  p=f"{c[-2]}{c[-1]}"  
+  while True:
+    if c[-1]+c[-2]<10:
+      c.append(c[-1]+c[-2])
+      p=f"{p}{c[-1]}"
+      if c[-1]==b and c[-2]==a:
+        print(p)
+        return p
+    elif c[-1]+c[-2]>=10:
+      d=c[-1]+c[-2]
+      e=1+(d-10)
+      c.append(e)
+      p=f"{p}{c[-1]}"
+      if c[-1]==b and c[-2]==a:
+        print(p)
+        return p
+    
   
-  return
-  str necklace number
-  """
-  return None
-
+  
 def main():
   assert necklace(9,4) == "94483257314595516742685494"
   assert necklace(1,3) == "13472922461786527977538213"
@@ -27,3 +39,33 @@ if __name__ == "__main__":
   main()
   
   
+  
+  
+  """while True:
+    if c[i]+c[i+1]<10:
+      c.append(c[i]+c[i+1])
+      print(c)
+      i+=1
+      if c[-1]==b and c[-2]==a:
+        break
+    elif c[i]+c[i+1]>=10:
+      d=c[i]+c[i+1]
+      e=1+(d-10)
+      c.append(e)
+      print(c)
+      i+=1
+      if c[-1]==b and c[-2]==a:
+        break"""
+
+  """for i in c:
+      print(i-1)"""
+    
+
+  """
+  inputs: 
+  a : int value [0..9]
+  b : int value [0..9]
+  
+  return
+  str necklace number
+  """
